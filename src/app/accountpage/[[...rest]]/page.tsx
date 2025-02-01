b22ef31d372cf684b0d038b4922fd7c20e2ea168
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-01 17:51:37 +0500

    first commit

--- src/app/accountpage/[[...rest]]/page.tsx
+++ src/app/accountpage/[[...rest]]/page.tsx
@@ -5,17 +5,14 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 export default function Page() {
   const page ="https://open-cow-26.accounts.dev/user"
-  
-          
 
 
   return (
     <section className="flex  flex-col items-center justify-center w-full h-screen">
-    <ClerkProvider>
       <SignIn afterSignInUrl={page} />
-    </ClerkProvider>
-    <Link href={"/studio"}>
-    <button className='bg-[#2A254B] py-4 px-6 rounded-[5px] text-[#F9F9F9]'>Click Here if you are Admin</button></Link>
+      <Link href={"/studio"}>
+        <button className='bg-[#2A254B] py-4 px-6 rounded-[5px] text-[#F9F9F9]'>Click Here if you are Admin</button>
+      </Link>
     </section>
   );
 }  
