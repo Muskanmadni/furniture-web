0857c88250a08978413ff179b212cd400af3deaa
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-03 03:53:46 +0500

    first commit

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-var server_1 = require("@clerk/nextjs/server");
+var server_1 = ("@clerk/nextjs/server");
 exports.default = (0, server_1.clerkMiddleware)();
 exports.config = {
     matcher: [
