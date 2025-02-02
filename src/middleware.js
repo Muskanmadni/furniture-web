cd54be3c80543591c12da6dbed6dba72ff18d4c2
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-02 16:34:09 +0500

    first commit

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,10 @@ exports.config = void 0;
 var server_1 = require("@clerk/nextjs/server");
 exports.default = (0, server_1.clerkMiddleware)();
 exports.config = {
-    matcher: ['/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpeg|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifes', '/(api|trpc)(.)'],
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
 };
-console.log("Clerk Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY); // Check if it's being read
-console.log("Clerk Secret Key:", process.env.CLERK_SECRET_KEY ? "Set" : "Not Set"); // Check if it's set (don't log the value!)
