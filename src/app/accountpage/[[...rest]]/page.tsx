a2fac45c2c1cfbd2eac1021b1bdc5afb7ac8b80c
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-02 13:08:28 +0500

    first commit

--- src/app/accountpage/[[...rest]]/page.tsx
+++ src/app/accountpage/[[...rest]]/page.tsx
@@ -1,18 +1,27 @@
+'use client';
 import Link from 'next/link';
-
-import { SignIn } from '@clerk/nextjs';
-
-
+import {SignIn}  from '@clerk/nextjs';
+import { useAuth } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
+import { AppProps } from 'next/app';
+ 
 export default function Page() {
-  const page ="https://open-cow-26.accounts.dev/user"
+  const page = "https://open-cow-26.accounts.dev/user";
 
 
   return (
-    <section className="flex  flex-col items-center justify-center w-full h-screen">
-      <SignIn afterSignInUrl={page} />
-      <Link href={"/studio"}>
-        <button className='bg-[#2A254B] py-4 px-6 rounded-[5px] text-[#F9F9F9]'>Click Here if you are Admin</button>
-      </Link>
-    </section>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <section className="flex flex-col items-center justify-center w-full h-screen">
+        <SignIn afterSignInUrl={page} />
+        <Link href="/studio">
+          <button className='bg-[#2A254B] py-4 px-6 rounded-[5px] text-[#F9F9F9]'>
+            Click Here if you are Admin
+          </button>
+        </Link> 
+      </section>
+    </ClerkProvider>
   );
-}  
+}
+
+
+
