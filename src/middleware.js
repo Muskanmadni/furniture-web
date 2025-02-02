df92d7b7d2081885712a8e7294b8b45576a07277
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-02 17:00:03 +0500

    first commit

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-var server_1 =("@clerk/nextjs/server");
-exports.default = (0, server_1.clerkMiddleware)();
+var clerkMiddleware_1 = require("../node_modules/@clerk/nextjs/dist/types/server/clerkMiddleware");
+exports.default = (0, clerkMiddleware_1.clerkMiddleware)({});
 exports.config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+        "/((?!.*\\..*|_next).*)",
+        "/",
+        "/(api|trpc)(.*)"
+    ]
 };
