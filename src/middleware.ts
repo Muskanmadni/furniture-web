652f47342a06930881a3b5bf817824483db1ebe1
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-03 03:38:25 +0500

    first commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,13 @@
-// middleware.js
-import { clerkMiddleware } from '@clerk/nextjs/server';
+"use client"
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-
-export default clerkMiddleware();
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones listed in the regex.
-     * This is the correct regex for middleware.
-     */
-    '/((?!api|_next|.*\\.(?:html|css|js|json|ico|svg|woff2?|ttf|eot|webmanifest|png|jpe?g|gif|bmp|tiff?|webp|pdf|txt))).*',
-    '/api/(.*)', // Match all API routes
-    '/trpc/(.*)', // Match all tRPC routes (if you're using tRPC)
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
-
-// Optional matcher for the /app directory (if using the App Router and have unprotected routes):
-// export const config = {
-//   matcher: [
-//     '/((?!api|_next|.*\\.(?:html|css|js|json|ico|svg|woff2?|ttf|eot|webmanifest|png|jpe?g|gif|bmp|tiff?|webp|pdf|txt))).*',
-//     '/api/(.*)',
-//     '/trpc/(.*)',
-//     '/app/((?!unprotected-page).*)', // Example: Exclude /app/unprotected-page
-//   ],
-// };
+}
